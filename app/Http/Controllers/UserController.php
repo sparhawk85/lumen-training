19906c073555dfee3427eb5f2f36c115976cd94e
Kamil Maraś <kamil.maras@bm.pl>
2016-05-20 23:30:22 +0200

    - szablony z innymi konstrukcjami

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('user.index', ['users' => User::all()]);
+    }
+}
